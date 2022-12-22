b92dc6e6f291a50013776bfb348de207b192989e
jayukasode23 <jayukasode2398@gmail.com>
2022-12-22 14:07:49 +0530

    Added Two Random Number Dice File

---
+++ TwoRandomNumberDice.js
@@ -0,0 +1,7 @@
+console.log("::::::::::UC3::::::::::::");
+console.log(":::::::::::::::Welcome to the JavaScript Programming Construct Program::::::::::::::");
+console.log("***Here We Use REPEL To  Get Two Random Dice Number ***");
+var Dice1 = Math.floor(Math.random()*6)+1;
+console.log("DiceNumber1:"  +   "   " + Dice1);
+var Dice2 = Math.floor(Math.random()*6)+1;
+console.log("DiceNumber2:"   + "  "+ Dice2);
