11ee2bcddd0f45650bd7530ceefe658959a35a3d
jayukasode23 <jayukasode2398@gmail.com>
2022-12-22 14:03:35 +0530

    Added Dice Number File

---
+++ DiceNumber.js
@@ -0,0 +1,6 @@
+console.log("::::::::::UC2::::::::::::");
+console.log(":::::::::::::::Welcome to the JavaScript Programing Construct Program::::::::::::::");
+console.log("***Here We Use REPEL To  Get Dice Number Between 1 To 6   ***");
+
+var Dice = Math.floor(Math.random()*6)+1;
+console.log(Dice);
