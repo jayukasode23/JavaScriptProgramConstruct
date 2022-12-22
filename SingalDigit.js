417a38b6c385f2cfbfb6613c5de3cc028f539777
jayukasode23 <jayukasode2398@gmail.com>
2022-12-22 14:01:31 +0530

    Added Single Digit Number File

---
+++ SingalDigit.js
@@ -0,0 +1,5 @@
+console.log("::::::::::UC1::::::::::::");
+console.log(":::::::::::::::Welcome to the JavaScript Programing Construct Program::::::::::::::");
+console.log("***Here We Use REPEL To  Get Single Digit Random function ***");
+
+console.log(Math.floor(Math.random() * 10));
